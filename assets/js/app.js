969de1bcd3e53492ca320973efe3ff1784dc45dc
AriellaRogueN <arrodriguezn@gmail.com>
2017-12-12 11:20:58 -0300

    Agregando Cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,29 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('#nav-btn').click(function() {
+       
+        $("#modal1").show();
+
+    });
+
+    $("#ad-btn").click(function() {
+        var nombre = $("#icon_prefix").val();
+        var telefono = $("#icon_telephone").val();
+
+        $(".container").append("<div class = 'col s12 m7'>" +
+            
+            "<div class = 'card horizontal'>" +
+            "<div class = 'card-stacked' >" +
+            "<div class = 'card-content' >" +
+            "<h4>"+ nombre + " </h4>" + 
+            "<p>" + telefono + "</p>" +
+            "</div></div></div></div>")
     
-    $("btn-floating").leanModal();
-  });
+    $("#modal1").hide();
+
+
+    })
+
+});
+
+
